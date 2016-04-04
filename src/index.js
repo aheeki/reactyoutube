7dca4ba52fbfc799e7e2a9b9540ae44bcd3d1b8e
Andrew Heekin <andrewheekin@gmail.com>
2016-04-04 12:44:17 -0400

    youtube search api call and app class

--- src/index.js
+++ src/index.js
@@ -1,16 +1,26 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import YTSearch from 'youtube-api-search';
 import SearchBar from './components/search_bar'; // I need to specify the path to import components I write
-
 const API_KEY = 'AIzaSyC4oB08Kr03e9qAYqgBy4bl23WEI8MD2lI';
 
-const App = () => {
-  return (
-  <div>
-    <SearchBar />
-  </div>
-  );
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { videos: [] };
+    YTSearch({ key: API_KEY, term: 'surfboards' }, function(data) {
+      console.log(data);
+    });    
+  }
+
+  render() {
+    return (
+    <div>
+      <SearchBar />
+    </div>
+    );
+  }
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
